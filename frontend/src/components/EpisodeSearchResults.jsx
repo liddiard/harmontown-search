67f9a8d6378779715961fb41d33dc0aaaa60fd57
Harrison Liddiard <omniaura5@gmail.com>
2023-10-15 12:51:08 -0400

    default episode search results to non-scrollable + add scroll button

--- frontend/src/components/EpisodeSearchResults.jsx
+++ frontend/src/components/EpisodeSearchResults.jsx
@@ -11,6 +11,10 @@ export default function EpisodeSearchResults({
   setCurrentEpisode
 }) {
   const [results, setResults] = useState([])
+  const [contentExceedsHeight, setContentExceedsHeight] = useState(false)
+  const [scrollable, setScrollable] = useState(false)
+  
+  const resultListEl = useRef(null)
   const fuse = useRef(null)
 
   useEffect(() => {
@@ -22,19 +26,33 @@ export default function EpisodeSearchResults({
   useEffect(() => {
     if (query && fuse.current) {
       setResults(fuse.current.search(query))
+      setScrollable(false)
+    }
+    if (resultListEl.current) {
+      resultListEl.current.scrollTop = 0
     }
   }, [query])
+
+  useEffect(() => {
+    const el = resultListEl.current
+    if (el) {
+      setContentExceedsHeight(el.scrollHeight > el.clientHeight)
+    }
+  }, [results])
 
   if (!results.length) {
     return null
   }
   return (
-    <>
+    <div className={s.results}>
       <h2>
         <span className="numResults">{results.length} </span>
         Episode description{results.length > 1 ? 's' : null}
       </h2>
-      <ol className={s.results}>
+      <ol 
+        className={scrollable ? s.scrollable : ''}
+        ref={resultListEl}
+      >
         {results.map(result => 
           <EpisodeResult
             key={result.item.number}
@@ -44,6 +62,15 @@ export default function EpisodeSearchResults({
             result={result.item} />
         )}
       </ol>
-    </>
+      {contentExceedsHeight && !scrollable ?
+        <button 
+          className={s.moreResults}
+          onClick={() => setScrollable(true)}
+        >
+          More episode results ⏷
+        </button>
+        : null
+      }
+    </div>
   )
 }
