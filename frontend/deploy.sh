e83d7faa1482d9c242dffd81983a16c1454c191a
Harrison Liddiard <omniaura5@gmail.com>
2023-10-23 20:42:35 +0900

    use absolute path for episodes symlink

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 BASE_DIR=$(pwd)
+PARENT_DIR="$(dirname "$BASE_DIR")"
 
-echo "Temporarily unlinking 'episodes' directory symlink"
+echo "⚠️  Temporarily unlinking 'episodes' directory symlink"
 unlink "$BASE_DIR/public/episodes"
 react-scripts build
 aws s3 sync build/ s3://harmontownsearch.com/ --acl public-read
-ln -s "$BASE_DIR/../episodes/" "$BASE_DIR/public"
-echo "Relinked 'episodes' directory symlink"
+ln -s "$PARENT_DIR/episodes/" "$BASE_DIR/public"
+echo "🟢 Relinked 'episodes' directory symlink"
