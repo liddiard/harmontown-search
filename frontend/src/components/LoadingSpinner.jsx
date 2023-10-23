97a51892089c85c9908c5b34b66ad16110949963
Harrison Liddiard <omniaura5@gmail.com>
2023-10-23 10:21:54 +0900

    fix issue w loading spinner rendering as '0'

--- frontend/src/components/LoadingSpinner.jsx
+++ frontend/src/components/LoadingSpinner.jsx
@@ -4,5 +4,5 @@ export default function LoadingSpinner({
   loading,
   className = ''
 }) {
-  return loading && <div className={`${s.loadingSpinner} ${className}`} />
+  return loading ? <div className={`${s.loadingSpinner} ${className}`} /> : null
 }
