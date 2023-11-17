083cdccafbe226adbfe216d9f4d3a33e9ee424c9
Harrison Liddiard <omniaura5@gmail.com>
2023-11-17 11:14:58 -0500

    add next app

---
+++ frontend/harmontown-search/next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
+  output: 'export'
+}
+
+module.exports = nextConfig
