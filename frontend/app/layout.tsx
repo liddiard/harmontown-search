762df8eaf65260b787ef48c3c3b32c63609918f6
Harrison Liddiard <omniaura5@gmail.com>
2023-12-24 22:25:18 -0700

    add ko-fi donation popup

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,11 +1,13 @@
 import type { Metadata } from 'next'
 import Image from 'next/image'
+import Link from 'next/link'
 import { Inter, Bebas_Neue } from 'next/font/google'
 import classNames from 'classnames'
 
 import 'layout.scss'
 import Nav from '@/components/nav'
 import heartIcon from 'img/heart.svg'
+import Donate from './components/Donate'
 
 
 const inter = Inter({
@@ -30,27 +32,31 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
 
-
   return (
     <html lang="en">
       <body className={inter.className}>
         <header>
-          <a href="/" id="logo">
+          <Link href="/" id="logo">
             <div className="unofficial">Unofficial</div>
             <h1 className={bebas.className}>
               Harmontown<span className={classNames(inter.className, 'searchHeading')}> Search</span>
             </h1>
-          </a>
+          </Link>
           <Nav />
         </header>
         {children}
         <footer>
-          <p className="donate">
-            <Image src={heartIcon} alt="love" /> Harmontown Search? <a href="https://www.buymeacoffee.com/liddiard" target="_blank" rel="noreferrer">Donate</a> toward the <a href="#">web hosting</a> to keep it running!
+          <p className="donation">
+            {/* Using an <a> element on the Donate button because the Kofi donate component relies
+                on the window.hashChange event, and this event is not fired for Next.js Link
+                component. The alternative would be to maintain donation popup open/close state
+                in this component, but that would entail making the entire app a client component. */}
+            <Image src={heartIcon} alt="love" /> Harmontown Search? <a href="#donate" className="donate">Donate</a> a few bucks to help fund the <Link href="#">web hosting costs</Link>!
           </p>
           <p className="disclaimer">
             This is a fan-made site. It is not affiliated with Harmontown Productions LLC nor with anyone involved in the podcast.
           </p>
+          <Donate />
         </footer>
       </body>
     </html>
