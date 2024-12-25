62b32536f9f294ba98a13c767bd2d2519f34b362
Harrison Liddiard <omniaura5@gmail.com>
2024-12-25 09:40:59 -0700

    add <time> tag to announcement banner

--- frontend/app/(search)/page.tsx
+++ frontend/app/(search)/page.tsx
@@ -4,9 +4,11 @@ import s from './page.module.scss'
 export default function Page() {
   return (
     <div className={s.banner}>
-      <strong>Update Dec 24, 2024:</strong> After a few months of missing videos
-      due to the closure of harmontown.com, video episode playback is back with
-      YouTube hosting –{' '}
+      <strong>
+        Update: <time dateTime="2024-12-25">Dec 25, 2024</time> 🎄
+      </strong>{' '}
+      After a few months of missing videos due to the closure of harmontown.com,
+      video episode playback is back with YouTube hosting –{' '}
       <Link href="/about/#thanks">thanks to these people</Link>!
     </div>
   )
