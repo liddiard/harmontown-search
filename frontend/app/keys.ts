24ecf3c1ee8330aee5e5234e196d00a196988392
Harrison Liddiard <git@harrisonliddiard.com>
2026-02-22 08:52:02 -0500

    update public api key for new server deployment

--- frontend/app/keys.ts
+++ frontend/app/keys.ts
@@ -2,5 +2,5 @@ export const TYPESENSE = {
   // Replace with your own key for local development (see /frontend/README.md)
   // Note: These keys are intended to be public, read-only keys, scoped to only allow search operations on the `transcripts` collection, created by `generate_scoped_search_key.sh`
   DEV: 'Mkl6RXBvQnR1Q2pNcitlZHdzR05XeHZTeGVHdk1BQm15eVMvY29EQnZIUT1ENHhoeyJxdWVyeV9ieSI6InRleHQiLCJncm91cF9ieSI6ImVwaXNvZGUiLCJncm91cF9saW1pdCI6MTAsInNvcnRfYnkiOiJlcGlzb2RlOmFzYyIsInVzZV9jYWNoZSI6dHJ1ZSwiY2FjaGVfdHRsIjozNjAwfQ==',
-  PROD: 'eUpwRFNJQXBhaDVoQ0Z1dkU3ZGRwUVlZRUc3aGxwbHJ1VXp5Uy9EdHZFOD10WllNeyJxdWVyeV9ieSI6InRleHQiLCJncm91cF9ieSI6ImVwaXNvZGUiLCJncm91cF9saW1pdCI6MTAsInNvcnRfYnkiOiJlcGlzb2RlOmFzYyIsInVzZV9jYWNoZSI6dHJ1ZSwiY2FjaGVfdHRsIjozNjAwfQ==',
+  PROD: 'bDdwU0lVMm41WHFyT3pITFA1V1FBVVFJMUQ2ZWwvUDE1a0JnUXNaL1dJdz0zTGFUeyJxdWVyeV9ieSI6InRleHQiLCJncm91cF9ieSI6ImVwaXNvZGUiLCJncm91cF9saW1pdCI6MTAsInNvcnRfYnkiOiJlcGlzb2RlOmFzYyIsInVzZV9jYWNoZSI6dHJ1ZSwiY2FjaGVfdHRsIjozNjAwfQ==',
 }
